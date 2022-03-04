eefeb555c74a3fff194f0bddc040b8941c5f228f
garrettbadger <garrett.badger.id@gmail.com>
2022-03-03 17:55:12 -0700

    added some comments

--- bankaccount/models.py
+++ bankaccount/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class BankAccounts(models.Model):
+    # return an integer field
     content = models.IntegerField('amount')
  
 
--- bankaccount/views.py
+++ bankaccount/views.py
@@ -5,24 +5,25 @@ from django.db.models.functions import Cast
 # Create your views here.
 
 def set_up(request):
+    #collect all bankaccounts objects
     all_bank_accounts = BankAccounts.objects.all()
-
+    #return the bankaccounts objects to the html in the form of a dictionary
     return render(request, 'setup.html', {'bank_accounts':all_bank_accounts})
     
 
 def addBankAccount(request):
+    #create another dictionary with all the bank account objects
     all_bank_accounts = BankAccounts.objects.all()
     bank_balance = {'statements': all_bank_accounts}
+    #collect the amount of money from the html document provided by the user
     new_amount = BankAccounts(content = int(request.POST['amount']))
-    
+    #save it to the db
     new_amount.save()
-    
+    #if the radio button for deposit was selected then add the current amount to the previous one
     if request.POST['changing_funds'] == 'Deposit':
         new_amount += Cast('integer',bank_balance['statements'])
+    #if the radio button for withdraw was selected then subtract the current amount from the previous one
     else:
          new_amount -= Cast('integer',bank_balance['statements'])
-
-    
-    
-    
+    #return to the html
     return HttpResponseRedirect('/bankaccount/')
